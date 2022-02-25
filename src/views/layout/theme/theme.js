94b646846e00e1bf08a705b6f8715e5aae87e214
Saffat55 <90462805+Saffat55@users.noreply.github.com>
2022-02-25 10:25:46 -0500

    Initialize feature/bootstrap

---
+++ src/views/layout/theme/theme.js
@@ -0,0 +1,24 @@
+// Import CSS
+import './theme.scss'
+
+// Vendor
+import 'lazysizes/plugins/object-fit/ls.object-fit'
+import 'lazysizes/plugins/parent-fit/ls.parent-fit'
+import 'lazysizes/plugins/rias/ls.rias'
+import 'lazysizes/plugins/bgset/ls.bgset'
+import 'lazysizes'
+import 'lazysizes/plugins/respimg/ls.respimg'
+
+// Polyfills
+import 'focus-within-polyfill'
+
+// Core
+import '@scripts/core/a11y'
+
+// Components
+import '@scripts/components/drawers'
+import '@scripts/components/cart'
+import '@scripts/components/required-validation'
+
+// Sections
+import '@sections/mobile-nav/mobile-nav'
